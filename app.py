609a4ff369693b7052ceea71165bcfa0e7b18596
Daren Cox <register@darencox.com>
2018-12-22 19:27:24 +0000

    Added postgress support

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 # source venv/bin/activate
 # deactivate to close the venv
 
+import os
 
 from flask import Flask
 from flask_restful import Api
@@ -16,7 +17,8 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# Heroku db env variable or sqlite for local backup
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'daren_cox_69'
 api = Api(app)
