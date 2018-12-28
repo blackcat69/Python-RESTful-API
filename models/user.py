6b9fdf2d2c0975e8204f8b41a2ffb247a11996c6
Daren Cox <register@darencox.com>
2018-12-28 16:56:41 +0000

    Updates for Section 11:

--- models/user.py
+++ models/user.py
@@ -12,11 +12,21 @@ class UserModel(db.Model):
 	def __init__(self, username, password):
 		self.username = username
 		self.password = password
+
+	def json(self):
+		return {
+			'id': self.id,
+			'username': self.username
+		}
 	
 	def save_to_db(self):
 		db.session.add(self)
 		db.session.commit()
 	
+	def delete_from_db():
+		db.session.delete(self)
+		db.session.commit()
+
 	@classmethod
 	def find_by_username(cls, username):
 		return cls.query.filter_by(username=username).first()
